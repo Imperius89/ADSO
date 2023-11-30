79d3d623e7608ed24d124afeba5c3d04d9a67a11
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-29 22:25:45 -0500

    c02

---
+++ C++/Clase02/Variables Enteras.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+int num = 34;
+int num2 = 5;
+int suma = num * num2;
+
+using namespace std;
+
+int main()
+{
+    cout << suma << "\n\n" << endl;
+    system("pause");
+    return 0;
+}
