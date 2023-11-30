db38ba2c5f51f43e25ab900e6288c9a8af8632a7
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-29 22:27:45 -0500

    classe 07

---
+++ C++/Clase07/Operadores Basicos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int operacion;
+
+int main ()
+{
+    operacion = 12;
+    operacion *= 3;
+    cout << operacion << endl;
+    getch();
+    return 0;
+
+}
