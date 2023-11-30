a1f87f392d28084f88f3bd3f5c41fb6bd123fd4b
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-29 22:26:04 -0500

    clase 03

---
+++ C++/Clase03/Variables Bool y Char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+bool variable = 34;
+
+int main ()
+{
+    cout << variable << "\n\n" << endl;
+    system("pause");
+    return 0;
+
+}
