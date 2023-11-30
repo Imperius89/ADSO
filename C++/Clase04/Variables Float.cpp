6f614d782d443d19c6dc08145c6b1aed7fe11a02
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-29 22:26:36 -0500

    clase05

---
+++ C++/Clase04/Variables Float.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main ()
+{
+    float num = 22.567156;
+    float num2 = 1.3;
+    cout << num + num2 << endl;
+    getch();
+    return 0;
+}
