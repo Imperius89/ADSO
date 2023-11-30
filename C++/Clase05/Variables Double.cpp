cf9a4e8527b2e8aaef62771a77e2223031772a24
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-29 22:26:52 -0500

    Clase 05

---
+++ C++/Clase05/Variables Double.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    double num = 0.000000000000000000000054884131584;
+    cout << num << endl;
+    getch();
+    return 0;
+}
