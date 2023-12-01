db8f1e62177a7b4a452e6ae0f176697251a5fe85
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-11-30 20:24:35 -0500

    Add files via upload

---
+++ HTML + CSS/Clase 26_09_2023/fechayHora.js
@@ -0,0 +1,7 @@
+let date = new Date();
+
+document.write(date.getDate()+"/0");
+document.write(date.getMonth()+1+"/");
+document.write(date.getFullYear()+", ");
+document.write(date.getHours()+":");
+document.write(date.getMinutes());
