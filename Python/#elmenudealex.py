dd05ef55f2e79e9b9c8056627ebcf3fcc637a74d
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-01 19:52:01 -0500

    Add files via upload

---
+++ Python/#elmenudealex.py
@@ -0,0 +1,19 @@
+#elmenudealex
+
+a=True
+print(a)
+while (a==True):
+    print(" EL MENU DE ALEX")
+    print("1.Manzana")
+    print("2.Banano")
+    print("3.Nada")
+    n=int(input())
+    if n==1:
+        print("Estas comiendo ",n," Manzana" )
+    elif n==2:
+        print("Estas comiendo ",n," Banano" )
+    elif n==3:
+        print("Estas comiendo ",n," Nada" )
+    else:
+        print("Numero no valido" )
+#print(a)
