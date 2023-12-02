d5a80b2586e94ef52c07a50919875226e2d9b423
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-01 19:35:40 -0500

    Add files via upload

---
+++ Java/02 Java Core/04variables.java
@@ -0,0 +1,24 @@
+public class variables{
+    public static void main(String[] args) {
+        
+        System.out.println("Estas son las variables");
+
+        int edad = 33;
+        System.out.println("Mi edad es : " + edad);
+        
+        float altura = 1.76f;
+        System.out.println("Mi altura es: " + altura);
+
+        double pi = 3.1415789876;
+        System.out.println("El valor de Pi es: " + pi);
+
+        long distancia = 31516781561849876l;
+        System.out.println("El numero largo es: " + distancia);
+
+        boolean bool = true;
+        System.out.println("El apuntador esta en: " + bool);
+
+        char inicial = 'O';
+        System.out.println("mi inicial es: " + inicial);
+    }
+}
