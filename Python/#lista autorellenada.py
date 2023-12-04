f8d419739a33387534c4eaa7d6aea9b2ea9a6714
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-03 19:59:57 -0500

    Add files via upload

---
+++ Python/#lista autorellenada.py
@@ -0,0 +1,12 @@
+#lista autorellenada 
+#print("Ingrese el tamaño de la lista")
+t=  int(input("Ingrese el tamaño de la lista"))
+lista= range(t)
+print(list(lista))
+print(tuple(lista))
+#open("")
+print(ord("@"))
+print(round(7/3))
+# a="-"
+j1= 0
+print("-".join(lista))
