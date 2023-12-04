dcbf85be9ffefc75278360a5a0425e18efa3b462
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-03 19:59:23 -0500

    Add files via upload

---
+++ Python/#if-else.py
@@ -0,0 +1,18 @@
+#if-else
+print("Ingrese el numero a verificar")
+
+a= 2+2
+if a==4:
+    print("El numero ingresado es igual a 4")
+else:
+    print("El numero ingresado es diferente de 4")
+
+b= 2+7
+if b==4:
+    print("El numero ingresado es igual a 4")
+elif b==5:
+    print("El numero ingresado es igual a 5")
+elif b==6:
+    print("El numero ingresado es igual a 6")
+else:
+    print("El numero ingresado no es igual a 4 5 o 6")
