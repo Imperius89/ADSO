d4011cfadca267bc102f436608a3467f1540b760
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-03 20:04:03 -0500

    Add files via upload

---
+++ Python/#tupla de colores.py
@@ -0,0 +1,7 @@
+#tupla de colores
+lista=("blanco","amarillo","azul","rojo")
+color= input("Ingrese el color a comparar")
+if color in lista:
+    print("El color ",color, " si esta")
+else:
+    print("El color ",color, " no esta")
