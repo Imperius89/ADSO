a8e95419d0a75d2da1f64c12e6fe8d278f47f0a0
Oscar David Becerra Mejia <150070997+Imperius89@users.noreply.github.com>
2023-12-03 20:03:48 -0500

    Add files via upload

---
+++ Python/#rellenar las listas.py
@@ -0,0 +1,15 @@
+#rellenar las listas
+invitados=[1,2,3,4,5]
+cc=  int(input("Ingrese el numero de la cedula del invitado"))
+if invitados[0]==cc:
+    print("El vale si esta invitado 0")
+elif invitados[1]==cc:
+    print("El vale si esta invitado 1")
+elif invitados[2]==cc:
+    print("El vale si esta invitado 2")
+elif invitados[3]==cc:
+    print("El vale si esta invitado 3")
+elif invitados[4]==cc:
+    print("El vale si esta invitado 4")
+else:
+    print("Seguridad")
